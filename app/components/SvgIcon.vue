2fc05514dcef0723ed1308d6bf44861dab3adac0
PolinaZharkova <polya.zharkowa@yandex.com>
2026-04-03 18:20:40 +0500

    initial commit

---
+++ app/components/SvgIcon.vue
@@ -0,0 +1,33 @@
+<template>
+    <component 
+    v-if="iconPath"
+    class="svg-icon"
+    :is="iconPath"
+    :width="width"
+    :height="height"
+    />
+</template>
+
+<script setup>
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    width: {
+        type: [String, Number],
+        default: undefined
+    },
+    height: {
+        type: [String, Number],
+        default: undefined
+    }
+});
+
+const icons = import.meta.glob('@/assets/svg-icons/*.svg')
+
+const iconPath = computed(() => {
+  const loader = icons[`/assets/svg-icons/${props.name}.svg`]
+  return loader ? defineAsyncComponent(loader) : null
+})
+</script>
